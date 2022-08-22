bbaff67a6ca0c66d9353ecd165103998b51f6c7a
XMEStudio <43714166+XMEStudio@users.noreply.github.com>
2022-08-22 23:31:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ def get_weather():
   return weather['weather'], math.floor(weather['temp'])
 
 def get_today():
-  delta = datetime.now().year
+  delta = datetime.now().year + "-" + datetime.now().month + "-" + datetime.now().day
   return delta
 
 def get_count():
